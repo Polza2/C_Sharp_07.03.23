bcaa7bc9a26efd01b1b41d5f074606dfee116cbf
Polza_2 <111ekh@mail.ru>
2023-03-09 16:25:54 +0300

    HW_Task_4

---
+++ Lesson_1/HW/Task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а
+// на выходе показывает все четные числа от 1 до N.
+
+Console.WriteLine("Please, enter number: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int count = 2;
+
+while (count < n)
+{
+  Console.WriteLine(count);
+  if (count < n)
+  {
+    count = count + 2;
+  }
+
+}
